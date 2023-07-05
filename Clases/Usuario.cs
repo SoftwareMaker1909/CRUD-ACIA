ad94c4dbf8623481a64138ff96063144dbf96c77
FRANCISCO ALBERTO PAULINO VALERIO <1106084@est.intec.edu.do>
2023-07-05 00:04:12 -0400

    Clase usuario

--- Clases/Usuario.cs
+++ Clases/Usuario.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace CRUD_ACIA.Clases
 {
-    internal class Class1
+    public class Usuario
     {
+        public int ID_Usuario { get; set; }
+        public string Cedula_Usuario { get; set; }
+        public string Nombre_Usuario { get; set; }
+        public string Apellido_Paterno_Usuario { get; set; }
+        public string Apellido_Materno_Usuario { get; set; }
+        public DateTime FechaNacimiento_Usuario { get; set; }
+        public int ID_Credencial { get; set; }
+        public int ID_Seccion { get; set; }
+        public string ResponsableInsert { get; set; }
+        public DateTime DateTimeInsert { get; set; }
+        public bool isenabled { get; set; }
     }
 }
